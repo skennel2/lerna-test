95b83b130ae84ab9cdb465e743e0108c504daa0b
skennel2 <skennel2@gmail.com>
2020-02-13 14:44:06 +0900

    initial commit

---
+++ packages/main-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import sub from '@mee/sub';
+
+function App() {
+  return (
+    <div className="App">
+      {sub()}
+    </div>
+  );
+}
+
+export default App;
